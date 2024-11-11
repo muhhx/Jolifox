637cd6c7f2322a92d392398c8875e952e1f1033e
Murilo <muriloue@gmail.com>
2024-11-11 15:07:47 -0300

    feat: create initial setup

---
+++ dist/bah/bah3/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.teste = void 0;
+const teste = "asd";
+exports.teste = teste;
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("module-alias/register");
+const bah3_1 = require("@/bah/bah3");
+console.log("Feito", bah3_1.teste);
